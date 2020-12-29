7904d0977a8aa5955cbb55b7506f03c8cbf1d6ed
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2020-12-29 10:24:34 +0700

    Add files via upload

--- Mymod.vb
+++ Mymod.vb
@@ -1,4 +1,6 @@
 ﻿Imports MySql.Data.MySqlClient
+Imports System.Security.Cryptography
+
 Module Mymod
     Public myCommand As New MySqlCommand
     Public myAdapter As New MySqlDataAdapter
@@ -17,8 +19,10 @@ Module Mymod
     'Tabel User
     '--------------------------------
     Public user_baru As Boolean
-    Public oUser As New Fakultas
+    Public oUser As New User
     '--------------------------------
+
+    Public login_valid As Boolean
 
 
     Public Sub DBConnect()
@@ -32,6 +36,19 @@ Module Mymod
     Public Sub DBDisconnect()
         cn.Disconnect()
     End Sub
+
+    Public Function getMD5Hash(ByVal strToHash As String) As String
+
+        Dim md5Obj As New System.Security.Cryptography.MD5CryptoServiceProvider()
+        Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
+        bytesToHash = md5Obj.ComputeHash(bytesToHash)
+        Dim strResult As String = ""
+        Dim b As Byte
+        For Each b In bytesToHash
+            strResult += b.ToString("x2")
+        Next
+        Return strResult
+    End Function
 End Module
 
 
